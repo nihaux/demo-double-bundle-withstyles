2160e4eee70b9799f67cabc6623408f0be910d52
nihau <nicolas.hauseux@gmail.com>
2018-05-29 21:42:42 +0200

    init

---
+++ test/demo-double-bundle-withstyles.test.ts
@@ -0,0 +1,14 @@
+import DummyClass from '../src/demo-double-bundle-withstyles'
+
+/**
+ * Dummy test
+ */
+describe('Dummy test', () => {
+  it('works if true is truthy', () => {
+    expect(true).toBeTruthy()
+  })
+
+  it('DummyClass is instantiable', () => {
+    expect(new DummyClass()).toBeInstanceOf(DummyClass)
+  })
+})
